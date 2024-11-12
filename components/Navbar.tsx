61523d9cb2d9688a7f4cc07e0a06cea20740619f
asifikbal09 <asif.ikbal662@gmail.com>
2024-11-13 03:38:06 +0600

    [DONE] Logo design complete.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,27 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
+import MobileNav from './MobileNav';
 
 const Navbar = () => {
     return (
-        <div>
-            Navbar
-        </div>
+        <nav className='flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
+            <Link className='flex items-center gap-1' href="/">
+            <Image 
+            src="/icons/logo.png"
+            width={32}
+            height={32}
+            alt='EchoMeet logo'
+            className='max-sm:size-10'
+            />
+            <p className='text-[26px] font-extrabold text-white max-sm:hidden'>EchoMeet</p>
+            </Link>
+            <div className='flex-between gap-5'>
+            {/* Clerk - User management */}
+
+            <MobileNav/>
+            </div>
+        </nav>
     );
 };
 
